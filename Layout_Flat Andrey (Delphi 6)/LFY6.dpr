a8e5e140401eb6d6b089d1c33162701841942d2b
Andrey Ramos <andreyranos@gmail.com>
2023-12-04 16:02:44 -0300

    Layout_Flat_Delphi_6
    Feito por Andrey Ramos

---
+++ Layout_Flat Andrey (Delphi 6)/LFY6.dpr
@@ -0,0 +1,13 @@
+program LFY6;
+
+uses
+  Forms,
+  ComponentesPadraoUnit in 'ComponentesPadraoUnit.pas' {ComponentesPadrao};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TComponentesPadrao, ComponentesPadrao);
+  Application.Run;
+end.
